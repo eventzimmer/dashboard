41a8ae9b4609d4e2a4535e583e79ad2a39b5f83e
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-09-14 14:19:15 +0200

    Add rudimentary login button.

---
+++ src/auth.js
@@ -0,0 +1,37 @@
+import auth0 from 'auth0-js'
+import EventEmitter from 'events';
+import authConfig from '@/assets/auth_config.json'
+
+const webAuth = new auth0.WebAuth({
+  domain: authConfig.domain,
+  redirectUri: `${window.location.origin}/callback`,
+  clientID: authConfig.clientId,
+  responseType: 'id_token',
+  scope: 'openid profile email'
+});
+
+class AuthService extends EventEmitter {
+
+  // Starts the user login flow
+  login(customState) {
+    webAuth.authorize({
+      appState: customState
+    });
+  }
+
+  // Handles the callback request from Auth0
+  handleAuthentication() {
+    return new Promise((resolve, reject) => {
+      webAuth.parseHash((err, authResult) => {
+        if (err) {
+          reject(err);
+        } else {
+          this.localLogin(authResult);
+          resolve(authResult.idToken);
+        }
+      });
+    });
+  }
+}
+
+export default new AuthService();
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <button type="button" @click="login">Take me to the login, sire!</button>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -32,10 +33,17 @@
 </template>
 
 <script>
+import AuthService from '@/auth'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    login () {
+      AuthService.login();
+    }
   }
 }
 </script>
